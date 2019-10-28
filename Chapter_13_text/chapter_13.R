d9c2c647e5aadd3eaa4e41bd923112084b408568
timbroderik <aabroder@gmail.com>
2019-10-27 20:43:01 -0500

    still working on regexp

--- Chapter_13_text/chapter_13.R
+++ Chapter_13_text/chapter_13.R
@@ -269,6 +269,56 @@ df$description <- str_replace_all(df$description,"\n"," ")
 write_csv(df,'test3.csv', na = '')
 # yes
 
+sql <- "SELECT * FROM crime_reports;"
+dbGetQuery(con, sql)
+
+# updating crime_reports table with extracted data
+sql <- "UPDATE crime_reports SET date_1 = (
+  (regexp_match(original_text, '\\d{1,2}\\/\\d{1,2}\\/\\d{2}'))[1]
+  || ' ' ||
+  (regexp_match(original_text, '\\/\\d{2}\\n(\\d{4})'))[1] 
+  ||' US/Eastern')::timestamptz;"
+  
+dbGetQuery(con, sql)
+
+sql <- "SELECT crime_id, date_1, original_text FROM crime_reports;"
+dfx <- dbGetQuery(con, sql)
+
+
+sql <- "UPDATE crime_reports 
+  SET date_1 = ((regexp_match(original_text, '\\d{1,2}\\/\\d{1,2}\\/\\d{2}'))[1]
+  || ' ' ||
+  (regexp_match(original_text, '\\/\\d{2}\\n(\\d{4})'))[1] 
+  ||' US/Eastern')::timestamptz,
+  date_2 = 
+  CASE 
+  WHEN (SELECT regexp_match(original_text, '-(\\d{1,2}\\/\\d{1,2}\\/\\d{1,2})') IS NULL)
+  AND (SELECT regexp_match(original_text, '\\/\\d{2}\\n\\d{4}-(\\d{4})') IS NOT NULL)
+  THEN 
+  ((regexp_match(original_text, '\\d{1,2}\\/\\d{1,2}\\/\\d{2}'))[1]
+  || ' ' ||
+  (regexp_match(original_text, '\\/\\d{2}\\n\\d{4}-(\\d{4})'))[1] 
+  ||' US/Eastern')::timestamptz 
+  WHEN (SELECT regexp_match(original_text, '-(\\d{1,2}\\/\\d{1,2}\\/\\d{1,2})') IS NOT NULL)
+  AND (SELECT regexp_match(original_text, '\\/\\d{2}\\n\\d{4}-(\\d{4})') IS NOT NULL)
+  THEN 
+  ((regexp_match(original_text, '-(\\d{1,2}\\/\\d{1,2}\\/\\d{1,2})'))[1]
+  || ' ' ||
+  (regexp_match(original_text, '\\/\\d{2}\\n\\d{4}-(\\d{4})'))[1] 
+  ||' US/Eastern')::timestamptz 
+  ELSE NULL 
+  END,
+  street = (regexp_match(original_text, 'hrs.\\n(\\d+ .+(?:Sq.|Plz.|Dr.|Ter.|Rd.))'))[1],
+  city = (regexp_match(original_text,'(?:Sq.|Plz.|Dr.|Ter.|Rd.)\\n(\\w+ \\w+|\\w+|\\w+)\\n'))[1],
+  crime_type = (regexp_match(original_text, '\\n(?:\\w+ \\w+|\\w+|\\w+)\\n(.*):'))[1],
+  description = (regexp_match(original_text, ':\\s(.+)(?:C0|SO)'))[1],
+  case_number = (regexp_match(original_text, '(?:C0|SO)[0-9]+'))[1];"
+dbGetQuery(con, sql)
+
+sql <- "SELECT case_number, date_1, street, city, crime_type, description
+FROM crime_reports;"
+dfy <- dbGetQuery(con, sql)
+
 # disconnect
 dbDisconnect(con)
 dbUnloadDriver(drv)
